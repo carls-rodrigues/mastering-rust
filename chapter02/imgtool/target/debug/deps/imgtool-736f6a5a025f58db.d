215d81f89a501f9d54fa2142d2367e825dfc9cc5
Carlos Rodrigues <cerf@furg.br>
2023-05-16 16:52:05 -0300

    imgtool

---
+++ chapter02/imgtool/target/debug/deps/imgtool-736f6a5a025f58db.d
@@ -0,0 +1,5 @@
+/home/cerf/mastering-rust/chapter02/imgtool/target/debug/deps/imgtool-736f6a5a025f58db: src/main.rs
+
+/home/cerf/mastering-rust/chapter02/imgtool/target/debug/deps/imgtool-736f6a5a025f58db.d: src/main.rs
+
+src/main.rs:
---
+++ chapter02/imgtool/target/debug/imgtool.d
@@ -0,0 +1,1 @@
+/home/cerf/mastering-rust/chapter02/imgtool/target/debug/imgtool: /home/cerf/mastering-rust/chapter02/imgtool/src/main.rs
