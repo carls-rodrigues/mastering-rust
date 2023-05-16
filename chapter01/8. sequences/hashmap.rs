943f40450453c7aac1706325ef2cc3393b43f6a3
Carlos Rodrigues <cerf@furg.br>
2023-05-16 00:32:28 -0300

    remove files from main branch

--- chapter01/8. sequences/hashmap.rs
+++
@@ -1,7 +0,0 @@
-use std::collections::HashMap;
-
-fn main() {
-    let mut configuration = HashMap::new();
-    configuration.insert("path", String::from("/home/user/"));
-    println!("Configured path is {:?}", configuration.get("path"));
-}
